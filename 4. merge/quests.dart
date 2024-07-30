03e051fc913856f0ccc9b79755531ca87c92a867
shinyeoungbin <youngbin03@hanyang.ac.kr>
2024-07-30 10:22:23 +0900

    first commit

---
+++ 4. merge/quests.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  int marbles = 0;
+  int branches = 0;
+
+  if(marbles > 30){
+    print('축하합니다! 구슬조각을 모두 모았습니다.');
+  }
+  if(branches > 50){
+    print('축하합니다! 나뭇가지를 모두 모았습니다.');
+  }
+  if(marbles > 30 && branches > 50){
+    print('축하합니다! 무기강화에 성공하셨습니다!');
+  }
+}
